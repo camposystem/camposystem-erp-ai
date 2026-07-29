4f3241b02097a4989f9276ba9c93c94f88590864
Alexandre Campos <campos0771@gmail.com>
2026-07-29 18:28:53 -0300

    feat(domain): add Entity, initial Product and ProductName value object

---
+++ src/CampoSystem.ErpAI.Domain/Entites/Products/Product.cs
@@ -0,0 +1,16 @@
+﻿using CampoSystem.ErpAI.Domain.Entites.Products.ValueObjects;
+
+namespace CampoSystem.ErpAI.Domain.Entites.Products;
+
+public sealed class Product: Entity
+{
+
+    public ProductName? Name { get; private set; }
+
+  //Description
+  //Sku
+  //Price
+  //IsActive
+
+
+}
