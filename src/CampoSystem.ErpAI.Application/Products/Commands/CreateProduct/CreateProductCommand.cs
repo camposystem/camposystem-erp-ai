253cf9caacf88439393683aa7b38a2d2fb24e7c9
Alexandre Campos <campos0771@gmail.com>
2026-09-14 17:08:24 -0300

    feat: add create product command

---
+++ src/CampoSystem.ErpAI.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace CampoSystem.ErpAI.Application.Products.Commands.CreateProduct;
+
+public sealed record CreateProductCommand(
+    string Name,
+    string Sku,
+    decimal Price,
+    string Description = "")
+{
+}
