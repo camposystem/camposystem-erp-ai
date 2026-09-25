1b1e89967087b97636c92c7bdcc8446573813e43
Alexandre Campos <campos0771@gmail.com>
2026-09-25 11:51:07 -0300

    feat(products): add EF Core repository and dependency injection

---
+++ src/CampoSystem.ErpAI.Infrastructure/Persistence/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using CampoSystem.ErpAI.Application.Products.Repositories;
+using CampoSystem.ErpAI.Domain.Products;
+
+namespace CampoSystem.ErpAI.Infrastructure.Persistence;
+
+public class ProductRepository : IProductRepository
+{
+
+    private readonly ProductDbContext _dbContext;
+
+    public ProductRepository(ProductDbContext dbContext)
+    {
+        _dbContext = dbContext; 
+    }
+
+    public Task<Product> AddAsync(Product product)
+    {
+        _dbContext.Products.Add(product);
+
+        _dbContext.SaveChanges();
+
+        return Task.FromResult(product);
+    }
+}
