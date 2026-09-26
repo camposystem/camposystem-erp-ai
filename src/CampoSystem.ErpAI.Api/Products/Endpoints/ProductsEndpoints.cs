f71e47da33bff85d91251e5f5d6ec6d30f55ec44
Alexandre Campos <campos0771@gmail.com>
2026-09-26 12:26:31 -0300

    feat(products): add create product endpoint

---
+++ src/CampoSystem.ErpAI.Api/Products/Endpoints/ProductsEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using CampoSystem.ErpAI.Api.Products.Mappers;
+using CampoSystem.ErpAI.Api.Products.Requests;
+using CampoSystem.ErpAI.Application.Products.Commands.CreateProduct;
+using CampoSystem.ErpAI.Application.Response;
+using CampoSystem.ErpAI.SharedKernel.Common.Result;
+
+namespace CampoSystem.ErpAI.Api.Products.Endpoints;
+
+public static class ProductsEndpoints
+{
+    public static void MapCreateProduct(WebApplication app)
+    {
+        app.MapPost("/api/products", async (CreateProductRequest request, CreateProductCommandHandler handler) =>
+        {
+            var command = ProductRequestMapper.ToCreateProductCommand(request);
+            var result = await handler.Handle(command);
+
+            if (result.IsSuccess)
+            {
+                return Results.Created("", (result.Value));
+            }
+
+            return Results.BadRequest(result.Errors);
+        }).Produces<CreateProductResponse>(201)
+        .Produces<IReadOnlyList<Error>>(400);
+    }
+}
---
+++ src/CampoSystem.ErpAI.Api/Products/Mappers/ProductRequestMapper.cs
@@ -0,0 +1,17 @@
+﻿using CampoSystem.ErpAI.Api.Products.Requests;
+using CampoSystem.ErpAI.Application.Products.Commands.CreateProduct;
+
+namespace CampoSystem.ErpAI.Api.Products.Mappers;
+
+public static class ProductRequestMapper
+{
+
+    public static CreateProductCommand ToCreateProductCommand(CreateProductRequest request)
+    {
+        return new CreateProductCommand(
+            request.Name,
+            request.Sku,
+            request.Price,
+            request.Description);
+    }
+}
